a512c7b47de9aec35355c7abe834a1090d9dc362
Faisal <faisy@hotmail.ca>
2022-03-27 22:11:27 -0600

    Created proper menu layout, added quit and about functions

--- src/main/java/mvh/app/MainController.java
+++ src/main/java/mvh/app/MainController.java
@@ -1,6 +1,8 @@
 package mvh.app;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import mvh.world.*;
 
 public class MainController {
@@ -13,6 +15,21 @@ public class MainController {
      */
     @FXML
     public void initialize() {
+    }
+
+    @FXML
+    void aboutProgram(ActionEvent event) {
+        Alert info = new Alert(Alert.AlertType.INFORMATION,"test");
+        String info_txt = "Author: Faisal Islam\nEmail: faisal.islam@ucalgary.ca\nVersion: 1.0\nA GUI interface to create and edit MvH worlds";
+        info.setContentText(info_txt);
+        info.setHeaderText("About");
+        info.setTitle("About Program");
+        info.show();
+    }
+
+    @FXML
+    void quitProgram(ActionEvent event) {
+        System.exit(0);
     }
 
 }
